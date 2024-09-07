89728406e9b5f0decb408f2f972f21a4f9cb271c
Hardik Pawar <hardikhpawar.cs21@rvce.edu.in>
2024-09-07 16:08:42 +0530

    Gitignore changes

--- automater.py
+++
@@ -1,9 +0,0 @@
-import FileHarvestor
-
-if __name__ == '__main__':
-    FileHarvestor.read_files(file_list=[
-        "./backend/server.js",
-        "./frontend/src/App.js",
-        "./frontend/src/components/VideoGameCard.js",
-        "./frontend/src/components/ReviewInput.js",
-    ])
